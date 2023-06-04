1603b40ceb7c0873c9d963d4d5b1cde77b6edbde
Jens Christian Wahl <jens.c.wahl@gmail.com>
2023-06-03 23:53:41 -0700

    run simulation

--- simulation_study.R
+++ simulation_study.R
@@ -1,10 +1,24 @@
 library(TMB)
 library(rstan)
-options(mc.cores = parallel::detectCores())
-rstan_options(auto_write = TRUE)
-
 library(data.table)
-library(worldfootballR)
 library(ggplot2)
 library(patchwork)
 theme_set(theme_bw())
+source("functions.R")
+
+options(mc.cores = parallel::detectCores())
+rstan_options(auto_write = TRUE)
+fig_dir = "figs/"
+n_games = c(5, 10, 20, 40, 80)
+stan_mod = stan_model("poisson_goals_model.stan")
+
+res = lapply(n_games, function(n_games) {
+  cat("Fitting for", n_games, "games\n")
+  data = simulate_games(n_games = n_games)
+  tmb_fit = fit_tmb(data$data)
+  stan_fit = fit_stan(data$data, stan_mod)
+  return(list(tmb = tmb_fit, 
+              stan = stan_fit))
+})
+
+saveRDS("sim_results.rds")
